48da8dbbbbacfaff2a6a0408cffa3aeaae11fdaa
NatikKazi <137032947+NatikKazi@users.noreply.github.com>
2025-06-13 12:04:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("enquiryForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  
+
   const formData = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
