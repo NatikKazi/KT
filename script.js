37bda3fcaa28e6b201a3f098b5be7dbeccfd1fe7
Natik Kazi <kazinatik5@gmail.com>
2025-06-13 13:11:00 +0530

    V1.2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.getElementById("enquiryForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -24,30 +23,3 @@ document.getElementById("enquiryForm").addEventListener("submit", function (e) {
     console.error(error);
   });
 });
-=======
-document.getElementById("enquiryForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const formData = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    phone: document.getElementById("phone").value,
-    message: document.getElementById("message").value
-  };
-
-  fetch("https://script.google.com/macros/s/AKfycbzJp5BF2KezSLm4LQkaegcRQtyRwf0bcrm4fgFS-iQ6lQXmIXQe-fet7yIUJiO5ZNc4/exec", {
-    method: "POST",
-    body: JSON.stringify(formData),
-    headers: { "Content-Type": "application/json" }
-  })
-  .then(response => response.text())
-  .then(data => {
-    alert("Thank you! Your enquiry has been submitted.");
-    document.getElementById("enquiryForm").reset();
-  })
-  .catch(error => {
-    alert("There was an error. Please try again later.");
-    console.error(error);
-  });
-});
->>>>>>> 48da8dbbbbacfaff2a6a0408cffa3aeaae11fdaa
